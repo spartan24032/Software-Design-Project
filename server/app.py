874c69fce29c9cb937d5a77d747c815f7a97e886
spartan24032 <118637167+spartan24032@users.noreply.github.com>
2024-02-22 15:23:35 -0600

    modified:   server/app.py

--- server/app.py
+++ server/app.py
@@ -1,9 +1,15 @@
 from flask import Flask, jsonify,render_template
 from flask_cors import CORS
+import os
+
+current_dir = os.path.dirname(__file__).strip('\server')+'\client\public'
+
+# template_folder = os.path.join(current_dir, '/public')
 
 
 # instantiate the app
-app = Flask(__name__,template_folder ='client/public/')
+app = Flask(__name__,template_folder=current_dir)
+#Get the absolute path from the folder
 app.config.from_object(__name__)
 
 # enable CORS

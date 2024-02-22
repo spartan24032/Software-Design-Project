a696e384ebcfb1b218ec4e0db2d616803c197c8b
spartan24032 <118637167+spartan24032@users.noreply.github.com>
2024-02-22 16:38:11 -0600

    new file:   client/templates/fuel_history.html 	modified:   client/templates/index.html 	new file:   client/templates/login.html 	renamed:    client/public/profile.html -> client/templates/profile.html 	renamed:    client/templates/price.html -> client/templates/quote_form.html 	new file:   client/templates/signup.html 	modified:   server/app.py

--- server/app.py
+++ server/app.py
@@ -22,25 +22,25 @@ def homepage():
 #/fuel_quote_form --> Joshua
 @app.route('/fuel_quote_form')
 def fuel_quote_form():
-    return render_template('price.html')
+    return render_template('quote_form.html')
 
 #fuel_quote_history --> Sahib
 @app.route('/fuel_quote_history')
 def fuel_quote_history():
-    return render_template('price.html')
+    return render_template('fuel_history.html')
 #
 @app.route('/client_profile_management')
 def profile_management():
-    return render_template('price.html')
+    return render_template('profile.html')
 
 #Login + Sign Up --> Vayeshnavi
 @app.route('/login')
 def loggin():
-    return render_template('price.html')
+    return render_template('login.html')
 
 @app.route('/signup')
 def sign_up():
-    return render_template('price.html')
+    return render_template('signup.html')
 
 if __name__ == '__main__':
     app.run()

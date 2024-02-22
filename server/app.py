4882507ac4327ae43a2c3258724f81b59917e5f0
spartan24032 <118637167+spartan24032@users.noreply.github.com>
2024-02-22 16:02:33 -0600

    new file:   client/public/swif.jpg 	new file:   client/templates/index.html 	renamed:    client/public/price.html -> client/templates/price.html 	modified:   server/app.py

--- server/app.py
+++ server/app.py
@@ -2,27 +2,44 @@ from flask import Flask, jsonify,render_template
 from flask_cors import CORS
 import os
 
-current_dir = os.path.dirname(__file__).strip('\server')+'\client\public'
+current_dir = os.path.dirname(__file__).strip('\server')
+path_current='\client'
+current_dir +=path_current
 
-# template_folder = os.path.join(current_dir, '/public')
 
 
 # instantiate the app
-app = Flask(__name__,template_folder=current_dir)
+app = Flask(__name__,template_folder=current_dir+r'\templates',static_folder =current_dir+r'\public')
 #Get the absolute path from the folder
 app.config.from_object(__name__)
 
-# enable CORS
-CORS(app, resources={r'/*': {'origins': '*'}})
 
 
-# sanity check route
-@app.route('/ping', methods=['GET'])
-def ping_pong():
-    return jsonify('pong!')
+@app.route('/')
+def homepage():
+    return render_template('index.html',image_filename='swif.jpg')
 
-@app.route('/price')
-def priced():
+#/fuel_quote_form --> Joshua
+@app.route('/fuel_quote_form')
+def fuel_quote_form():
+    return render_template('price.html')
+
+#fuel_quote_history --> Sahib
+@app.route('/fuel_quote_history')
+def fuel_quote_history():
+    return render_template('price.html')
+#
+@app.route('/client_profile_management')
+def profile_management():
+    return render_template('price.html')
+
+#Login + Sign Up --> Vayeshnavi
+@app.route('/login')
+def loggin():
+    return render_template('price.html')
+
+@app.route('/signup')
+def sign_up():
     return render_template('price.html')
 
 if __name__ == '__main__':

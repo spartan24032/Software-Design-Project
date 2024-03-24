0c055009f30759eaa6d06915c8040d32e6fafb7f
sebgutierrez <84500161+sebgutierrez@users.noreply.github.com>
2024-03-23 23:27:33 -0500

    Fixed comments for consistency

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,15 +3,15 @@ import os
 from flask_app import create_app # look in __init__.py file for more info
 from app import app
 
-# this file contains pytest fixtures: helper functions that can be passed to other test functions
+# This file contains pytest fixtures: helper functions that can be passed to other test functions
 
 @pytest.fixture()
 def test_client():
 	
-	# configure app for testing. refer to config.py for more info
+	# Configure app for testing
     app.config.from_object('config.TestingConfig')
 
-    # test_client() is a Flask method that makes requests to the application without running a live server. has methods that mimick HTTP methods like client.get() and client.post()
+    # test_client() is a Flask method that makes requests to the application without running a live server. Has methods that mimick HTTP methods like client.get() and client.post()
     with app.test_client() as testing_client:
         """
 			The use of 'with' is used for resource management. 
@@ -19,11 +19,11 @@ def test_client():
             Both test_client and app_context() support the use of 'with'. 
             They can be considered as context managers.
         """
-        # for more info on app_context(): https://flask.palletsprojects.com/en/2.3.x/appcontext/ 
+        # For more info on app_context(): https://flask.palletsprojects.com/en/2.3.x/appcontext/ 
         with app.app_context():
             """
 				Yield is used to return an object like a regular return statement. 
-				The difference is that the code after yield will continue executing after the function calling this function (app()) finishes. 
-			 	In this instance, it's so that functions calling test_client_app remain in the app_context()
+				The difference is that the code after yield will continue executing after the function calling this function finishes. 
+			 	In this instance, it's so that functions calling test_client remain in the app_context()
 			"""
             yield testing_client
--- tests/test_profile.py
+++ tests/test_profile.py
@@ -3,12 +3,12 @@ from flask_app.forms.profile_form import ProfileForm
     
 def test_profile_get(test_client):
 	response = test_client.get('/profile')
-	# test if get request is successful
+	# Test if get request is successful
 	assert response.status_code == 200
 
-# TODO: Add Not Applicable option for address 2 in form
+# TODO: Add 'Not Applicable' option for address 2 in form.
 def test_profile_post(test_client):
-	# test with valid form data. populating form fields and assigning to the 'data' argument
+	# Test with valid form data. Populating the form fields by passing in a dictionary to 'data'.
 	response = test_client.post(
 		'/profile',
 		data={'name': 'President', 'address1': '1600 Pennsylvania Avenue, N.W.', 'address2': 'Not Applicable', 'city': 'Washington', 'state': 'DC', 'zipcode': '20500'},
@@ -17,5 +17,5 @@ def test_profile_post(test_client):
 	# follow_redirects: Make additional requests to follow HTTP redirects until a non-redirect status is returned.
 	# Check that there was one redirect response.
 	assert len(response.history) == 1
-	# Check that the second request was to the index page.
+	# Check that the redirect request was for the profile page.
 	assert response.request.path == "/profile"
